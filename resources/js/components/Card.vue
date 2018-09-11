635743883ce447224044aff8985f712c63caf79c
Wahyu Kristianto <w.kristories@gmail.com>
2018-09-11 15:22:22 +0700

    Money formating

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -47,7 +47,6 @@ export default {
     },
     methods: {
         formatPrice(value) {
-            return value
             let val = (value/1).toFixed(2).replace('.', ',')
             return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
         }
