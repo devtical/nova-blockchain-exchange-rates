4fa73f91e2cef78234f983da85b2476511ebae57
Wahyu Kristianto <w.kristories@gmail.com>
2018-09-11 14:48:58 +0700

    Added new route

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,16 @@
 <?php
+
+use Zttp\Zttp;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Route;
+
+Route::get('ticker', function (Request $request) {
+    $data = Cache::remember('blockchain-exchange-rates::ticker', 10, function () {
+        $response = Zttp::get('https://blockchain.info/ticker');
+
+        return $response->json();
+    });
+
+    return response()->json($data);
+});
